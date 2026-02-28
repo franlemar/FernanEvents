9063a2853fbd8865e61ad073fc0591e08904db16
Chema <chemamoralcampanario.2004@gmail.com>
2026-02-28 23:23:38 +0100

    Metodos de interfaz aumentable y CRUD con manejo de funciones de fechas

--- controlador/GestionEvento.java
+++ controlador/GestionEvento.java
@@ -1,6 +1,11 @@
 package FernanEvents.controlador;
 
+import FernanEvents.modelo.CategoriaEvento;
 import FernanEvents.modelo.Evento;
+import FernanEvents.modelo.Usuario;
+import FernanEvents.modelo.utilidades.FuncionesFechas;
+
+import java.time.LocalDate;
 
 public class GestionEvento {
 
@@ -27,4 +32,85 @@ public class GestionEvento {
     public void setNumEventos(int numEventos) {
         this.numEventos = numEventos;
     }
+
+    //*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.MÉTODOS INTERFAZ AUMENTABLE.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*
+    public void aumentarCapacidad(){
+        aumentarCapacidad(1);
+    }
+
+    public void aumentarCapacidad(int cantidad){
+        Evento[] nuevoArray = new Evento[eventos.length + cantidad];
+        for (int i = 0; i < numEventos; i++) {
+            nuevoArray[i] = eventos[i];
+        }
+        this.eventos = nuevoArray;
+    }
+
+    public void disminuirCapacidad(){
+        disminuirCapacidad(1);
+    }
+
+    public void disminuirCapacidad(int cantidad){
+        int nuevoTamanio = eventos.length - cantidad;
+        if(nuevoTamanio < numEventos){
+            System.out.println("ERROR, no se puede reducir ese espacio");
+            return;
+        }
+        Evento[] nuevoArray = new Evento[nuevoTamanio];
+
+        for (int i = 0; i < numEventos; i++) {
+            nuevoArray[i] = eventos[i];
+        }
+        this.eventos = nuevoArray;
+    }
+
+    //*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.CRUD.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*
+    //C --> CREATE
+    public boolean aniadirEvento (Evento nuevoEvento){
+        if (nuevoEvento == null) return false;
+
+        if (buscarEventoPorNombre(nuevoEvento.getNombre()) != null){
+            return false;
+        }
+
+        if (numEventos == eventos.length){
+            aumentarCapacidad(5);
+        }
+
+        eventos[numEventos++] = nuevoEvento;
+        return true;
+    }
+
+    public Evento crearEvento (String nombre, String descripcion, CategoriaEvento categoria, String fecha, int aforo, int personasInscritas){
+        LocalDate fechaEvento = FuncionesFechas.convertirStringEnFecha(fecha);
+        if (fechaEvento == null) return null;
+
+        Evento nuevoEvento = new Evento(nombre, descripcion, categoria, fechaEvento, aforo, personasInscritas);
+
+        if (aniadirEvento(nuevoEvento)) {
+            return nuevoEvento;
+        }
+        return null;
+
+    }
+
+    //----------------------------------------------------------------------------------------------------
+    //R --> READ
+    public Evento buscarEventoPorNombre (String nombre){
+        for (int i = 0; i < numEventos; i++) {
+            if (eventos[i] != null && eventos[i].getNombre().equalsIgnoreCase(nombre)){
+                return eventos[i];
+            }
+        }
+        return null;
+    }
+    //U --> UPDATE
+    //D --> DELETE
+
+
+
+
+
+
+
 }
