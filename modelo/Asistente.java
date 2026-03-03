6619d026bb32cc97e2e2d76c9f930e7d27b1ff16
Chema <chemamoralcampanario.2004@gmail.com>
2026-03-03 11:08:05 +0100

    en Evento.java correoOrganizador añadido y metodos nuevos. En Asistente.java 2 metodos nuevos para las entradas

--- modelo/Asistente.java
+++ modelo/Asistente.java
@@ -43,4 +43,22 @@ public class Asistente extends Usuario implements Bloqueable {
     public boolean estaBloqueado() {
         return this.bloqueado;
     }
+
+    //metodos para las entradas
+    public void aniadirEntrada(Entrada entrada){
+        if (numEntradas < misEntradas.length){
+            misEntradas[numEntradas] = entrada;
+            numEntradas++;
+        }
+    }
+
+    //obtener el total de entradas
+    public int getTotalEntradasCompradas(){
+        int total=0;
+        for (int i = 0; i < numEntradas; i++) {
+            total= total+ misEntradas[i].getCantidad();
+        }
+        return total;
+    }
+
 }
--- modelo/Evento.java
+++ modelo/Evento.java
@@ -13,10 +13,13 @@ public class Evento {
     private int personasInscritas;
     private int id;
     private static int contadorId = 0; //aquí he puesto static para que todos los objetos compartan el mismo contador
+    private String correoOrganizador;
 
+    private EntradasTipo[] tiposEntrada;
+    private int numTipos;
 
     //Constructor
-    public Evento(String nombre, String descripcion, CategoriaEvento categoriaEvento, LocalDate fecha, int aforo, int personasInscritas) {
+    public Evento(String nombre, String descripcion, CategoriaEvento categoriaEvento, LocalDate fecha, int aforo, int personasInscritas, String correoOrganizador) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.categoriaEvento = categoriaEvento;
@@ -25,6 +28,8 @@ public class Evento {
         this.personasInscritas = personasInscritas;
         this.id = contadorId;
         contadorId++;
+        this.correoOrganizador = correoOrganizador;
+        this.tiposEntrada = new EntradasTipo[3];
     }
 
     public String getNombre() {
@@ -78,4 +83,44 @@ public class Evento {
     public int getId() { return id; }
 
     public void setId(int id) { this.id = id; }
+
+    public EntradasTipo[] getTiposEntrada() {return tiposEntrada;}
+
+    public void setTiposEntrada(EntradasTipo[] tiposEntrada) {this.tiposEntrada = tiposEntrada;}
+
+    public int getNumTipos() {return numTipos;}
+
+    public void setNumTipos(int numTipos) {this.numTipos = numTipos;}
+
+    public String getCorreoOrganizador() {return correoOrganizador;}
+
+    public void setCorreoOrganizador(String correoOrganizador) {this.correoOrganizador = correoOrganizador;}
+
+    //añadir un tipo, solo se usa al crear el evento
+    public void aniadirTipoEntrada(String nombre, float precio, int cantidad){
+        if (numTipos < 3){
+            tiposEntrada[numTipos++] = new EntradasTipo(nombre, precio, cantidad);
+        }
+    }
+
+    public EntradasTipo getTipoEntrada(String nombreTipo) {
+        for (int i = 0; i < numTipos; i++) {
+            if (tiposEntrada[i].getNombre().equalsIgnoreCase(nombreTipo)) {
+                return tiposEntrada[i];
+            }
+        }
+        return null;
+    }
+
+    public boolean venderEntradas(String nombreTipo, int cantidad){
+        EntradasTipo tipo = getTipoEntrada(nombreTipo);
+        if (tipo != null && tipo.getCantidadDisponible() >= cantidad){
+            tipo.setCantidadDisponible(tipo.getCantidadDisponible() + cantidad);
+
+            this.personasInscritas = personasInscritas + cantidad;
+            return true;
+        }
+        return false;
+    }
+
 }
